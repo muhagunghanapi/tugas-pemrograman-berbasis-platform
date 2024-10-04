ff6e7d8b84a80554f60ec9f89975e47eb9e6e016
muhagunghanapi <161696731+muhagunghanapi@users.noreply.github.com>
2024-10-04 10:52:50 +0700

    Add files via upload
    tugas 2 sesi 2

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// Menghitung Keliling Lingkaran
+// const jariJari = prompt('masukan jari-jari lingkaran :');
+
+// const pi = 3.14;
+
+// const Keliling = jariJari*pi*2;
+
+// console.log('keliling lingkaran nya adalah : '+ Keliling + 'cm');
+
+// Menghitung luas persegi panjang
+// const panjang = prompt('masukan panjang :');
+// const lebar = prompt('masukan lebar');
+
+// const luas = panjang * lebar ;
+
+// console.log('luas persegi adalah : ' + luas);
+
+// menghitung volume kubus
+// const sisi = prompt('masukan sisi kubus:');
+
+// const volume = sisi*sisi*sisi;
+
+// console.log(`volume kubus adalah ${volume}`);
+
+// menghitung dan menampilan suhu
+// const suhu = prompt('masukan suhu : ');
+
+// const farensheit = (suhu * 9/5) + 32;
+
+// console.log(`suhu dalam Farensheit adalah : ${farensheit}`);
+
+// menghitung uang kembalian dari total telur
+const telur = prompt('masukan jumlah terlur kg');
+const bayar = prompt('masukan total bayar');
+
+const hargaTelur = 28000;
+
+const total = telur * hargaTelur;
+const kembalian = total - bayar;
+
+console.log('jumlah telur : ' + telur + 'kg');
+console.log('total bayar : ' + bayar);
+console.log(`kembalian nya : ${kembalian}`);
+
+
