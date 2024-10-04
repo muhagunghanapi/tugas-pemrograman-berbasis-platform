b8e41e74436d9d27e9d7556a1f599c184ecc2323
muhagunghanapi <161696731+muhagunghanapi@users.noreply.github.com>
2024-10-04 10:47:56 +0700

    Add files via upload
    Tugas 1 sesi 2

---
+++ app.js
@@ -0,0 +1,13 @@
+// FUNCTION
+function tambah(a,b) {
+    return hasil = a + b;
+}
+console.log(tambah(3,4));
+
+// ARROW FUNCTION 
+const output = (a,b) => a + b;
+console.log(output(4,2));
+
+// Perbedaan 
+// function : menggunakan kata kunci function dan memiliki sintaksis panjang.
+// arrow function : fungsi nya sama tapi lebih ringkas dan lebih banyak menggunakan operator ternary.
